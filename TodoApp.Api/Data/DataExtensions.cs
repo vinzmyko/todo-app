f4a4427ecfcad07bb669662b3180898b8ff86ebc
Vinz <vinzmykodelrosario@gmail.com>
2024-09-12 15:26:20 +0100

    feat(api): add auto-migration functionality
    Automatically applies any pending database migrations when application starts, ensuring entities and database schema are up-to-date.

---
+++ TodoApp.Api/Data/DataExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TodoApp.Api.Data;
+
+public static class DataExtensions
+{
+    public static void MigrateDb(this WebApplication app) // Makes it extend from WebApplication
+    {
+        using var scope = app.Services.CreateScope();
+        var dbContext = scope.ServiceProvider.GetRequiredService<TodoAppDbContext>();
+        dbContext.Database.Migrate();
+    }
+}
