28efea2f1c50e6cf1b52287120ea53fd5802a716
Vinz <vinzmykodelrosario@gmail.com>
2024-09-15 21:26:35 +0100

    feat(web): add User models and API client

---
+++ TodoApp.Web/Clients/UsersClient.cs
@@ -0,0 +1,30 @@
+﻿using System.Net.Http.Json;
+using TodoApp.Web.Models;
+
+namespace TodoApp.Web.Clients;
+
+public class UsersClient
+{
+    private readonly HttpClient _httpClient;
+
+    public UsersClient(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<UserDto[]> GetUsersAsync()
+        => await _httpClient.GetFromJsonAsync<UserDto[]>("users") ?? [];
+
+    public async Task<UserDto> GetUserAsync(int id)
+        => await _httpClient.GetFromJsonAsync<UserDto>($"users/{id}")
+            ?? throw new Exception("Could not find user!");
+
+    public async Task CreateUserAsync(CreateUserDto user)
+        => await _httpClient.PostAsJsonAsync("users", user);
+
+    public async Task UpdateUserAsync(int id, CreateUserDto user)
+        => await _httpClient.PutAsJsonAsync($"users/{id}", user);
+
+    public async Task DeleteUserAsync(int id)
+        => await _httpClient.DeleteAsync($"users/{id}");
+}
---
+++ TodoApp.Web/Models/UserDtos.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TodoApp.Web.Models;
+
+public class UserDto
+{
+    public int Id { get; set; }
+    public required string Username { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
+
+public class CreateUserDto
+{
+    [Required]
+    [StringLength(50)]
+    public required string Username { get; set; }
+}
