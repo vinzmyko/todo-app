c4171e4a789c09d3c873551c9a3c88acad2d0894
Vinz <vinzmykodelrosario@gmail.com>
2024-09-15 21:25:50 +0100

    feat(web): configure API connection and logging

--- TodoApp.Web/Program.cs
+++ TodoApp.Web/Program.cs
@@ -1,10 +1,50 @@
 using TodoApp.Web.Components;
+using TodoApp.Web.Clients;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Set up logging
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+
+// Create a logger
+var loggerFactory = LoggerFactory.Create(logging =>
+{
+    logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
+    logging.AddConsole();
+});
+var logger = loggerFactory.CreateLogger<Program>();
+
+logger.LogInformation("Starting application");
+logger.LogInformation($"Environment: {builder.Environment.EnvironmentName}");
+
+// Add user secrets in development
+if (builder.Environment.IsDevelopment())
+{
+    logger.LogInformation("Adding user secrets to configuration");
+    builder.Configuration.AddUserSecrets<Program>();
+}
+
+var todoAppApiUrl = builder.Configuration["ApiSettings:BaseUrl"];
+logger.LogInformation($"API URL from configuration: {todoAppApiUrl}");
+
+if (string.IsNullOrEmpty(todoAppApiUrl))
+{
+    logger.LogError("BaseUrl is not set in ApiSettings");
+    throw new Exception("BaseUrl is not set in ApiSettings");
+}
+
+// Add services to the container
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddHttpClient<UsersClient>(client => 
+{
+    logger.LogInformation($"Configuring HttpClient with BaseAddress: {todoAppApiUrl}");
+    client.BaseAddress = new Uri(todoAppApiUrl);
+});
 
 var app = builder.Build();
 
@@ -12,16 +52,21 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+    app.UseHttpsRedirection();
 }
-
-app.UseHttpsRedirection();
+else
+{
+    app.UseDeveloperExceptionPage();
+    logger.LogInformation("HTTPS Redirection is disabled in Development environment");
+}
 
 app.UseStaticFiles();
 app.UseAntiforgery();
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
+
+logger.LogInformation("Application configured and ready to run");
 
 app.Run();
